cee25d37dd3f37bac10495da4a796400b926c6f1
meghakedia28 <32675620+meghakedia28@users.noreply.github.com>
2018-01-29 20:26:57 +0530

    added datatable

--- assign_10/js/facultyResultSet.js
+++ assign_10/js/facultyResultSet.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+	$('#quiz').DataTable();
+	$('#result').DataTable();
 	$('#submitForm').click(function(){
 		var postData = $('form').serialize();
 		$.ajax({
@@ -9,18 +11,19 @@ $(document).ready(function(){
 			success : function(result){
 				var slNo = 0;
 				var row = "";
-				title = "<tr><th>Sl No.</th><th>Quiz Name</th><th>Start date time</th><th>End date time</th><th>Student's name</th><th>Score percentage</th><th>Rank</th></tr>"
+				title = "<thead><tr><th>Sl No.</th><th>Quiz Name</th><th>Start date time</th><th>End date time</th><th>Student's name</th><th>Score percentage</th><th>Rank</th></tr></thead>"
 				var obj = $.parseJSON(result);
 				 $.each(obj, function( index, value ) {
 					 slNo = slNo + 1;
-                     row = row +"<tr><td>" + slNo + "</td><td>" + value.NAME + "</td><td>" + value.STARTDATETIME +
+                     row = row +"<tbody><tr><td>" + slNo + "</td><td>" + value.NAME + "</td><td>" + value.STARTDATETIME +
                     		 "</td><td>" + value.ENDDATETIME + "</td><td>" + value.FIRSTNAME + " " + value.LASTNAME +
-                    		 "</td><td>" + (parseFloat(value.SCORE / 100.00)*100.00).toFixed(2) + " %" + "</td><td>" + value.RANK + "</td></tr>" ;
+                    		 "</td><td>" + (parseFloat(value.SCORE / 100.00)*100.00).toFixed(2) + " %" + "</td><td>" + value.RANK + "</td></tr></tbody>" ;
                      
                   });
 				 $("#filterTable").html('');
 				 if (row != ''){
 					 $("#result").hide();
+					 $('#filterTable').DataTable();
 					 $("#filterTable").append(title+row);
 				 }
 				 else{
