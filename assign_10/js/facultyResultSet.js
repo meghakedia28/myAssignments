debfdea3e41769e8952e5ed0bb5e9420da6a9bda
meghakedia28 <32675620+meghakedia28@users.noreply.github.com>
2018-02-20 22:51:38 +0530

    removed the html datatable and added ajax datatable

--- assign_10/js/facultyResultSet.js
+++ assign_10/js/facultyResultSet.js
@@ -1,35 +1,74 @@
+var resultTable,filter, filtereData;
 $(document).ready(function(){
-	$('#quiz').DataTable();
-	$('#result').DataTable();
-	$('#submitForm').click(function(){
-		var postData = $('form').serialize();
-		$.ajax({
-			url : "../components/facultyResultSet.cfc?method=filterOnQuiz",
-			data : {
-				arrayOfQuizId : "&"+postData
-			},
-			success : function(result){
-				var slNo = 0;
-				var row = "";
-				title = "<thead><tr><th>Sl No.</th><th>Quiz Name</th><th>Start date time</th><th>End date time</th><th>Student's name</th><th>Score percentage</th><th>Rank</th></tr></thead>"
-				var obj = $.parseJSON(result);
-				 $.each(obj, function( index, value ) {
-					 slNo = slNo + 1;
-                     row = row +"<tbody><tr><td>" + slNo + "</td><td>" + value.NAME + "</td><td>" + value.STARTDATETIME +
-                    		 "</td><td>" + value.ENDDATETIME + "</td><td>" + value.FIRSTNAME + " " + value.LASTNAME +
-                    		 "</td><td>" + (parseFloat(value.SCORE / 100.00)*100.00).toFixed(2) + " %" + "</td><td>" + value.RANK + "</td></tr></tbody>" ;
-                     
-                  });
-				 $("#filterTable").html('');
-				 if (row != ''){
-					 $("#result").hide();
-					 $('#filterTable').DataTable();
-					 $("#filterTable").append(title+row);
-				 }
-				 else{
-					 $("#filterTable").html("No student has given this test");
-				 }
-              },
-			});
-		});
+	var userId = $('#userId').val();
+	resultTable = $('#result').DataTable({
+        "ajax": {
+			"url" : "../components/facultyResultSet.cfc?method=getResultSet",
+			"data" :{
+				userId : userId
+					}
+				}
+		 });
+	var buttons = new $.fn.dataTable.Buttons(resultTable, {
+		buttons: [
+		          {
+				    extend: 'csvHtml5',
+				    exportOptions: {
+				    columns: [ 0, 1, 2, 3, 4, 5, 6]
+				   }
+				},
+				{
+					extend: 'pdfHtml5',
+				    exportOptions: {
+				    columns: [ 0, 1, 2, 3, 4, 5, 6]
+				  }
+			   }
+			]
+		}).container().appendTo($('#buttons'));	
+	filter = $('#filterList').DataTable({
+		"lengthChange": false,
+		"paging": false,
+		"ajax": {
+			"url" : "../components/facultyResultSet.cfc?method=getListOfQuiz",
+			"data" :{
+				userId : userId
+					}
+				}
+		 });
 	});
+	function addFilter(data){
+		 var postData = [];
+         $.each($("form input[name='quizId']:checked"), function(){            
+        	 postData.push($(this).val());
+         });
+		var userId = $('#userId').val();
+		postData = postData.join(","),
+		resultTable.destroy();
+		resultTable = $('#result').DataTable({
+			"ajax": {
+				"url" : "../components/facultyResultSet.cfc?method=filterOnQuiz",
+				"data" : {
+					listOfQuizId : postData,
+					userId : userId
+					}       
+				}
+			});
+			var buttons = new $.fn.dataTable.Buttons(resultTable, {
+				buttons: [
+				          {
+						    extend: 'csvHtml5',
+						    exportOptions: {
+						    columns: [ 0, 1, 2, 3, 4, 5, 6]
+						   }
+						},
+						{
+							extend: 'pdfHtml5',
+						    exportOptions: {
+						    columns: [ 0, 1, 2, 3, 4, 5, 6]
+						  }
+					   }
+					]
+				}).container().appendTo($('#buttons'));	
+			$('.close').click();
+			}
+	
