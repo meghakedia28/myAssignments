325a9dd31a0e3c72a757614ab7308c4dab624760
meghakedia28 <32675620+meghakedia28@users.noreply.github.com>
2018-02-27 23:22:13 +0530

    remove the repeated codes

--- demoApplication/js/forgetPassword.js
+++ demoApplication/js/forgetPassword.js
@@ -23,6 +23,12 @@ $(document).ready(function(){
                             }
                         });
 					}
+					else if (obj.STATUS == "error"){
+						$.alert({
+							title: 'Error',
+                            content: 'Some Unexpected error has occured, please try again later.'
+						});
+					}
 					else {
 						$("#error_email").text(obj.MESSAGE);
 						return false;
@@ -39,26 +45,3 @@ $(document).ready(function(){
 		$('#error_email').text("");
 	});
 });			
-function emailCheck(elementId,errorId) {
-	var id = $(elementId).val();
-	var regid = /^[a-zA-Z][a-zA-Z0-9\s,'-.!@#\$%\^&\*]{8,40}$/;
-	var atposition = id.indexOf("@");
-	var dotposition = id.lastIndexOf(".");
-	if (id == "" || id == null){
-		$(errorId).text("You can't leave this empty.");
-		$(elementId).css("border","2px solid red");
-		return false;
-	}
-	if (!(regid.test(id))){
-		$(errorId).text("Please use alphabets (a-z) or (A-Z) between 8 and 30 characters");
-		$(elementId).css("border","2px solid red");
-		return false;
-	}
-	if (atposition < 1 || dotposition < atposition+2 || dotposition+2 >= id.length){
-		$(errorId).text("It should be of the format 'abc@gmail.com'");
-		$(elementId).css("border","2px solid red");
-		return false;
-	}
-	else 
-		return true;
-}
