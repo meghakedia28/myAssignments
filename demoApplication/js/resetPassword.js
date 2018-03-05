55401012cc4ff06b366198d8979a597ec8942202
meghakedia28 <32675620+meghakedia28@users.noreply.github.com>
2018-03-05 11:04:48 +0530

    removed the repeated codes to form a function for showing up error messages

--- demoApplication/js/resetPassword.js
+++ demoApplication/js/resetPassword.js
@@ -45,13 +45,11 @@ function passwordOut(){
 	var paswrd = $("#password").val();
 	var regpassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,30})/;
 	if (paswrd == "" || paswrd == null){
-		$("#error_password").text("You can't leave this empty.");
-		$("#password").css("border","2px solid red");
+		showErrorMessage("#password", "#error_password", "You can't leave this empty.");
 		return false;
 	}
 	else if (!(regpassword.test(paswrd))){
-		$("#error_password").html("<p>Enter a valid password it should contain:<br>One capital letter,<br>One small letter,<br>One special character,<br>One number,and<br>between 8 to 30 characters.<p>");
-		$("#password").css("border","2px solid red");
+		showErrorMessage("#password", "#error_password", "<p>Enter a valid password it should contain:<br>One capital letter,<br>One small letter,<br>One special character,<br>One number,and<br>between 8 to 30 characters.<p>");
 		return false;
 	}
 	else 
@@ -61,17 +59,19 @@ function c_passwordOut(){
 	$("#confirmPassword").css("backgroundColor","");
 	var paswrd = $("#password").val();
 	var cpaswrd = $("#confirmPassword").val();
-	//var regpassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/;
 	if (cpaswrd == "" || cpaswrd == null){
-		$("#error_confirmpassword").text("You can't leave this empty.");
-		$("#confirmPassword").css("border","2px solid red");
+		showErrorMessage("#confirmPassword", "#error_confirmpassword", "You can't leave this empty.");
 		return false;
 	}
 	else if (paswrd != cpaswrd){
-		$("#error_confirmpassword").text("Confirm password did not match with the password.");
-		$("#confirmPassword").css("border","2px solid red");
+		showErrorMessage("#confirmPassword", "#error_confirmpassword", "Confirm password did not match with the password.");
 		return false;
 	}
 	else
 		return true;
+}
+function showErrorMessage (elementId, errorId, errorMessage){
+	$(errorId).html(errorMessage);
+	$(elementId).css("border","2px solid red");
+	
 }
--- demoApplication/js/wordEmailNumberValidation.js
+++ demoApplication/js/wordEmailNumberValidation.js
@@ -2,13 +2,11 @@ function wordCheck(elementId,errorId){
 	var word = $(elementId).val();
 	var regword = /^[a-zA-Z]{1,30}$/;
 	if ( word == "" || word == null){
-		$(errorId).text("You can't leave this empty.");
-		$(elementId).css("border","2px solid red");
+		showErrorMessage(elementId, errorId, "You can't leave this empty.");
 		return false;
 	}
 	else if (!(regword.test(word))){
-		$(errorId).html("<p>Please use only letters(a-z) or (A-Z)<br/>between 1 and 30 characters.</p>");
-		$(elementId).css("border","2px solid red");
+		showErrorMessage(elementId, errorId, "<p>Please use only letters(a-z) or (A-Z)<br/>between 1 and 30 characters.</p>");
 		return false;
 	}
 	else
@@ -20,18 +18,15 @@ function emailCheck(elementId,errorId){
 	var atposition = id.indexOf("@");
 	var dotposition = id.lastIndexOf(".");
 	if (id == "" || id == null){
-		$(errorId).text("You can't leave this empty.");
-		$(elementId).css("border","2px solid red");
+		showErrorMessage(elementId, errorId, "You can't leave this empty.");
 		return false;
 	}
 	if (!(regid.test(id))){
-		$(errorId).text("Please use alphabets (a-z) or (A-Z) between 8 and 30 characters");
-		$(elementId).css("border","2px solid red");
+		showErrorMessage(elementId, errorId, "Please use alphabets (a-z) or (A-Z) between 8 and 30 characters");
 		return false;
 	}
 	if (atposition < 1 || dotposition < atposition+2 || dotposition+2 >= id.length){
-		$(errorId).text("It should be of the format 'abc@gmail.com'");
-		$(elementId).css("border","2px solid red");
+		showErrorMessage(elementId, errorId, "It should be of the format 'abc@gmail.com'");
 		return false;
 	}
 	else 
@@ -42,15 +37,17 @@ function numberCheck(elementId,errorId){
 	$(elementId).css("backgroundColor","");
 	var regnum = /^[0-9]{10}$/;
 	if (number1 == "" || number1 == null){
-		$(errorId).text("You can't leave this empty.");
-		$(elementId).css("border","2px solid red");
+		showErrorMessage(elementId, errorId, "You can't leave this empty.");
 		return false;
 	}
 	else if(!(regnum.test(number1))){
-		$(errorId).text("Please enter only numbers(0-9) of 10 digits.");
-		$(elementId).css("border","2px solid red");
+		showErrorMessage(elementId, errorId, "Please enter only numbers(0-9) of 10 digits.");
 		return false;
 	}
 	else
 		return true;
+}
+function showErrorMessage(elementId, errorId, errorMessage){
+	$(errorId).html(errorMessage);
+	$(elementId).css("border","2px solid red");
 }
