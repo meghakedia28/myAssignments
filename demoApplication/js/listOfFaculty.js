ae0fc0bbc1175540e7ab5aaeb4ef7879e8bcb162
meghakedia28 <32675620+meghakedia28@users.noreply.github.com>
2018-03-05 19:16:06 +0530

    js for client side processing to display datatable

---
+++ demoApplication/js/listOfFaculty.js
@@ -0,0 +1,112 @@
+var listOfFacultyTable;
+$(document).ready(function() {
+	var userid = $('#id').val() ;
+	 listOfFacultyTable = $('#listOfFaculties').DataTable({
+        "ajax": {
+			"url" : "../components/getUserDetailsService.cfc?method=getFacultyList",
+			"data" :{}
+    		}
+		 });
+	var buttons = new $.fn.dataTable.Buttons(listOfFacultyTable, {
+	buttons: [
+	          {
+			    extend: 'csvHtml5',
+			    title: 'List of faculties',
+			    exportOptions: {
+			    columns: [ 0, 1, 2, 3, 4]
+			   }
+			},
+			{
+				extend: 'pdfHtml5',
+				title: 'List of faculties',
+			    exportOptions: {
+			    columns: [ 0, 1, 2, 3, 4]
+			  }
+		   }
+		]
+	}).container().appendTo($('#buttons'));
+});		
+$(document).on('click','#edit', function(e){
+	e.preventDefault();
+	var userId = $(this).data('id');
+	$.ajax({
+		url : "../components/listOfFacultiesService.cfc?method=populateData",
+		data : {id : userId},
+		datatype: "html",
+	}).done(openEditModel);
+});
+function openEditModel(html){
+	$('#editModal').html('');
+	$('#editModal').html(html);
+	validate ('#editModal');
+}
+function validate(ref){
+	$("#submitEditForm").click(function(){
+		event.preventDefault();
+		//var valid = validateAll();
+		if(true){
+			submitForm();
+		}
+	});
+//	$("input").focus(function(){
+//		$(this).css("border","");
+//		$(this).next('.error-msg').text("");
+//	});
+//	$("#firstName, #lastName").focusout(function(){
+//		wordCheck(this,$(this).next('.error-msg'));
+//	});
+//	$("#contactNumber").focusout(function(){
+//		numberCheck('#contactNumber','#error_contactnumber');
+//	});
+//	$("#subject").focusout(function(){
+//		subjectCheck('#subject','#error_subject');
+//	});
+}
+function submitForm(){
+	event.preventDefault();
+		$.ajax({
+			url : "../components/enterDataService.cfc?method=updateUserInformationController&"+$("form").serialize(),
+			data : {},
+				success : function(result){
+					var obj = $.parseJSON(result);
+					if (obj.SUCCESSFULL != null){						
+						if(obj.SUCCESSFULL){
+							$(".error-msg").text("");
+							$('.close').click(); 
+							listOfFacultyTable.ajax.reload();
+						}
+					}
+					if (obj.ERRORID != null){
+						for (keys in obj.ERRORID){
+							var id = '#'+(keys.toLowerCase());
+							$(id).text(obj.ERRORID[keys]);
+						}
+					}
+				}
+		}) ;
+}
+function subjectCheck(elementId,errorId){
+	var subjectVal = $(elementId).val();
+	var validate = wordCheck(elementId,errorId);
+}
+function validateAll(){
+	var firstName = wordCheck('#firstName','#error_firstname');
+	var lastName = wordCheck('#lastName','#error_lastname');
+	var phoneNumber = numberCheck('#contactNumber','#error_contactnumber');
+	if ($('#subject').length != 0){
+		var sub = wordCheck('#subject','#error_subject');
+	}
+	if (firstName && lastName && phoneNumber){
+		if ($('#subject').length != 0){
+			if (sub)
+				return true;
+			else
+				return false;
+		}
+		else{
+			return true;
+		}
+	}
+	else
+		return false;
+}
---
+++ demoApplication/js/listOfStudent.js
@@ -0,0 +1,93 @@
+var listOfStudentTable;
+$(document).ready(function() {
+	var userid = $('#id').val() ;
+	 listOfStudentTable = $('#listOfStudents').DataTable({
+		"ajax": {
+			"url" : "../components/getUserDetailsService.cfc?method=getStudentsList",
+			"data" :{}
+    		}
+		});
+	var buttons = new $.fn.dataTable.Buttons(listOfStudentTable, {
+	buttons: [
+	          {
+			    extend: 'csvHtml5',
+			    title: 'List of students',
+			    exportOptions: {
+			    columns: [ 0, 1, 2, 3]
+			   }
+			},
+			{
+				extend: 'pdfHtml5',
+				title: 'List of students',
+			    exportOptions: {
+			    columns: [ 0, 1, 2, 3]
+			  }
+		   }
+		]
+	}).container().appendTo($('#buttons'));
+});
+$(document).on('click','#edit', function(e){
+	e.preventDefault();
+	var userId = $(this).data('id');
+	$.ajax({
+		url : "../components/listOfStudentsService.cfc?method=populateData",
+		data : {id : userId},
+		datatype: "html",
+	}).done(openEditModel);
+});
+function openEditModel(html){
+	$('#editModal').html('');
+	$('#editModal').html(html);
+	validate ('#editModal');
+}
+function validate(ref){
+	$("#submitEditForm").click(function(){
+		event.preventDefault();
+		var valid = validateAll();
+		if(valid){
+			submitForm();
+		}
+	});
+	$("input").focus(function(){
+		$(this).css("border","");
+		$(this).next('.error-msg').text("");
+	});
+	$("#firstName, #lastName").focusout(function(){
+		wordCheck(this,$(this).next('.error-msg'));
+	});
+	$("#contactNumber").focusout(function(){
+		numberCheck('#contactNumber','#error_contactnumber');
+	});
+}
+function submitForm(){
+	event.preventDefault();
+		$.ajax({
+			url : "../components/enterDataService.cfc?method=updateUserInformationController&"+$("form").serialize(),
+			data : {},
+				success : function(result){
+					var obj = $.parseJSON(result);
+					if (obj.SUCCESSFULL != null){						
+						if(obj.SUCCESSFULL){
+							$(".error-msg").text("");
+							$('.close').click(); 
+							listOfStudentTable.ajax.reload();
+						}
+					}
+					if (obj.ERRORID != null){
+						for (keys in obj.ERRORID){
+							var id = '#'+(keys.toLowerCase());
+							$(id).text(obj.ERRORID[keys]);
+						}
+					}
+				}
+		}) ;
+}
+function validateAll(){
+	var firstName = wordCheck('#firstName','#error_firstname');
+	var lastName = wordCheck('#lastName','#error_lastname');
+	var phoneNumber = numberCheck('#contactNumber','#error_contactnumber');
+	if (firstName && lastName && phoneNumber)
+		return true;
+	else
+		return false;
+}
