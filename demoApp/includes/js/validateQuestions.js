4cb3da16c8e2e92707d42aaf4fa7ca922371d632
meghakedia28 <32675620+meghakedia28@users.noreply.github.com>
2018-04-02 11:45:54 +0530

    added headers and changed the questions/options size from 50 to 255

--- demoApp/includes/js/validateQuestions.js
+++ demoApp/includes/js/validateQuestions.js
@@ -1,36 +1,61 @@
-function validate(elementId){
+/*-------------------------------------------------------------------------------------------------------------
+						FileName    : validateQuestions.js
+						Created By  : Megha Kedia
+						DateCreated : 13-March-2018
+						Description : does validation for add questions.
+
+--------------------------------------------------------------------------------------------------------------*/
+
+/*--------------------------------------------------------------------------------------
+Function Name  : validate
+Description    : validate all the fields for add questions
+Arguments	   : elementId
+Return Type	   : none
+------------------------------------------------------------------------------------------*/
+
+function validate(elementId, errorId){
 	 var value = $(elementId).val();
 		if (value == "" || value == null ){
-			showErrorMessage(elementId, "You can't leave this empty.");
+			showErrorMessage(elementId, errorId, "You can't leave this empty.");
 			return false;
 		}
-		else if (value.length < 1 || value.length > 50) {
-			showErrorMessage(elementId, "Please enter characters of length between 1 to 50.");
+		else if (value.length < 1 || value.length > 255) {
+			showErrorMessage(elementId, errorId, "Please enter characters of length between 1 to 255.");
 			return false;
 		}
 		else 
 			return true;
 }
+
+/*--------------------------------------------------------------------------------------
+Function Name  : checkUnique
+Description    : validate if all the options are unique or not.
+Arguments	   : optiona,
+ 				optionb,
+ 				optionc,
+ 				optiond
+Return Type	   : boolean
+------------------------------------------------------------------------------------------*/
+
 function checkUnique(optiona, optionb, optionc, optiond){
 	var error = 0;
 	if (optiond == optionc || optiond == optionb || optiond == optiona){
-		showErrorMessage("#optiond", "This option is already selected. Please enter a different option.");
+		showErrorMessage("#optiond", "error_optiond",
+				"This option is already selected. Please enter a different option.");
 		error = 1;
 	}
 	if (optionc == optionb || optionc == optiona){
-		showErrorMessage("#optionc", "This option is already selected. Please enter a different option.");
+		showErrorMessage("#optionc", "error_optionc",
+				"This option is already selected. Please enter a different option.");
 		error = 1;
 	}
 	if  (optionb == optiona){
-		showErrorMessage("#optionb", "This option is already selected. Please enter a different option.");
+		showErrorMessage("#optionb", "error_optionb",
+				"This option is already selected. Please enter a different option.");
 		error = 1;
 	}
 	if(error == 0)
 		return true;
 	else
 		return false;
-}
-function showErrorMessage (id, errorMessage){
-	$(id).css("border","2px solid red");
-	$(id).next(".error-msg").text(errorMessage);	
 }
