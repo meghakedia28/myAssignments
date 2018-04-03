0f6ca72de66712970abd275b9776d5fabe1df23d
meghakedia28 <32675620+meghakedia28@users.noreply.github.com>
2018-04-03 13:08:36 +0530

    removed unneccessary comments

--- demoApp/includes/js/addQuestions.js
+++ demoApp/includes/js/addQuestions.js
@@ -1,63 +1,75 @@
+/*-------------------------------------------------------------------------------------------------------------
+FileName    : addQuestions.js
+Created By  : Megha Kedia
+DateCreated : 13-March-2018
+Description : Question form submission using Ajax.
+
+--------------------------------------------------------------------------------------------------------------*/
+
 $(document).ready(function(){
 	$("#questionsForm").submit(function(event){
 		 event.preventDefault();
-//		 var checkQuestion = validate('#question');
-//		 var checkOptiona = validate('#optiona');
-//		 var checkOptionb = validate('#optionb');
-//		 var checkOptionc = validate('#optionc');
-//		 var checkOptiond = validate('#optiond');
-//		 var checkAnswer = validate('#answer');
-//		 var optiona = $('#optiona').val();
-//		 var optionb = $('#optionb').val();
-//		 var optionc = $('#optionc').val();
-//		 var optiond = $('#optiond').val();
-//		 var unique = false;
-//		 if ( optiona != "" && optionb != "" && optionc != "" && optiond != ""){
-//				unique = checkUnique(optiona, optionb, optionc, optiond);
-//			}
-		 if(true) {
-			 var url = "../?event=faculty.enterQuestions&"+$("#questionsForm").serialize();
-			 $.ajax ({
-			 	url : url,
-				data : {},
-					success: function(result){
-					var obj = $.parseJSON(result);
-					if (obj.SUCCESSFULL != null){						
-						if(obj.SUCCESSFULL){
-							$.confirm({
-	                            title: 'Success!',
-	                            content: obj.MESSAGE,
-	                            buttons: {
-	                                Ok : function () {
-	            							$("#questionsForm").trigger('reset');
-	            							$(".error-msg").text("");
-	            						}	
-	                                }
-	                            
-	                        });	
-						}
-						else{
-							$.confirm({
-	                            title: 'Error!',
-	                            content: obj.MESSAGE
-							});
-						}
-					}
-					if (obj.ERRORID != null){
-						for (keys in obj.ERRORID){
-							var id = '#'+(keys.toLowerCase());
-							$(id).text(obj.ERRORID[keys]);
-						}
-					}		
-				}
-			 });
+ 		 var checkQuestion = validate('#question', '#error_question');
+ 		 var checkOptiona = validate('#optiona', '#error_optiona');
+ 		 var checkOptionb = validate('#optionb', '#error_optionb');
+ 		 var checkOptionc = validate('#optionc', '#error_optionc');
+ 		 var checkOptiond = validate('#optiond', '#error_optiond');
+ 		 var checkAnswer = validate('#answer', '#error_answer');
+ 		 var optiona = ($('#optiona').val()).trim();
+ 		 var optionb = ($('#optionb').val()).trim();
+ 		 var optionc = ($('#optionc').val()).trim();
+ 		 var optiond = ($('#optiond').val()).trim();
+ 		 var unique = false;
+ 		 if ( optiona != "" && optionb != "" && optionc != "" && optiond != ""){
+ 				unique = checkUnique(optiona, optionb, optionc, optiond);
+ 			}
+		 if(checkQuestion && checkOptiona && checkOptionc && checkOptiond && checkOptionb && checkAnswer && unique){
+			var url = "../?event=faculty.enterQuestions&";
+			gobalAjaxHandler(url, $("#questionsForm").serializeArray(), addQuestionStatus);
 		 }
 	});
 	$(".inputField").focus(function(){
 		$(this).css("border","");
 		$(this).next('.error-msg').text("");
 	});
-//	$(".inputField").focusout(function(){
-//		validate(this);
-//	});
+ 	$(".inputField").focusout(function(){
+ 		validate(this, $(this).next('.error-msg'));
+ 	});
 });
+
+/*------------------------------------------------------------------------------------------
+Function Name  : addQuestionStatus
+Description    : success function which is called after ajax form submission for questions
+Arguments	   : result
+Return Type	   : none
+------------------------------------------------------------------------------------------*/
+
+function addQuestionStatus(result){
+	var obj = $.parseJSON(result);
+	if (obj.SUCCESSFULL != null){						
+		if(obj.SUCCESSFULL){
+			$.confirm({
+                title: 'Success!',
+                content: obj.MESSAGE,
+                buttons: {
+                    Ok : function () {
+							$("#questionsForm").trigger('reset');
+							$(".error-msg").text("");
+					}	
+                }
+            });	
+		}
+		else{
+			$.confirm({
+                title: 'Error!',
+                content: obj.MESSAGE
+			});
+		}
+	}
+	if (obj.ERRORID != null){
+		for (keys in obj.ERRORID){
+			var id = '#'+(keys.toLowerCase());
+			$(id).text(obj.ERRORID[keys]);
+		}
+	}		
+}
